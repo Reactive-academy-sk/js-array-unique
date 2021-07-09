1d4fb9b6bd6052cf7ab4e9925ef1ba4a7b04802a
patrikmasiar <masiar.patrik@gmail.com>
2021-07-09 16:58:13 +0200

    Add function to try catch

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ function getOnlyUniqueItems(value, index, self) {
 }
 
 module.exports = function unique(inputArray, key = null) {
-  if (!Array.isArray(inputArray)) {
-    throw new TypeError('Function unique() expects the first argument "inputArray" to be a type of array.');
-  }
-
-  if (key !== null) {
-    if (typeof key === 'string') {
-      var items = [...inputArray];
-      var itemsByKey = inputArray
-        .map(i => i[key])
-        .filter(getOnlyUniqueItems);
-
-      return itemsByKey
-        .map(itemKey => items.find(item => item[key] === itemKey));
-    } else {
-      throw new TypeError('Function unique() expects the second argument "key" to be a type of string.');
+  try {
+    if (!Array.isArray(inputArray)) {
+      throw new TypeError('Function unique() expects the first argument "inputArray" to be a type of array.');
     }
+  
+    if (key !== null) {
+      if (typeof key === 'string') {
+        var items = [...inputArray];
+        var itemsByKey = inputArray
+          .map(i => i[key])
+          .filter(getOnlyUniqueItems);
+  
+        return itemsByKey
+          .map(itemKey => items.find(item => item[key] === itemKey));
+      } else {
+        throw new TypeError('Function unique() expects the second argument "key" to be a type of string.');
+      }
+    }
+  
+    return inputArray.filter(getOnlyUniqueItems);
+  } catch(e) {
+    return [];
   }
-
-  return inputArray.filter(getOnlyUniqueItems);
 };
